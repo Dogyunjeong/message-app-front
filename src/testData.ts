e8e16dd6f1eb4a51a074dbbb408065720cc05ba5
Dogyun Jeong <dogun098@gmail.com>
2020-10-25 22:38:29 +0900

    test: add bit more test data

--- src/testData.ts
+++ src/testData.ts
@@ -2,6 +2,8 @@ import * as ChatTypes from './types/Chat.type'
 
 const DUMMY_USERS = [
     { uuid: 'test-user-1', name: 'James' },
+    { uuid: 'test-user-2', name: 'Timothy' },
+    { uuid: 'test-user-3', name: 'Max' },
 ]
 
 export const TEST_USER_UUID = "test-user-uuid";
@@ -9,8 +11,18 @@ export const TEST_USER_UUID = "test-user-uuid";
 
 export const ChatDummyData: ChatTypes.ChatRoom[] = [
     {
-        id: 'room-1',
+        id: 'room-0',
         members: [DUMMY_USERS[0]],
+        messages: []
+    },
+    {
+        id: 'room-1',
+        members: [DUMMY_USERS[1]],
+        messages: []
+    },
+    {
+        id: 'room-2',
+        members: [DUMMY_USERS[2]],
         messages: []
     }
 ]
